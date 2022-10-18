4eade1fb8f6cc649198ccdb6efa0327c5e60fe9c
mrsono0 <mrsono0@gmail.com>
2022-10-18 11:46:11 +0900

    TodoApp new TodiItem test ok

--- src/TodoApp_mrsono0.js
+++ src/TodoApp_mrsono0.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
@@ -15,8 +15,19 @@ const TodoApp_mrsono0 = () => {
             done: true,
         }
     ]);
+    const nextId = useRef(3);
+    const onInsert = useCallback(text => {
+        setTodos(
+            todos.concat({
+                id: nextId.current,
+                text,
+                done: false,
+            })
+        );
+        nextId.current += 1;
+    }, [todos]);
     return (<div>
-        <TodoForm />
+        <TodoForm onInsert={onInsert} />
         <TodoList todos={todos} />
     </div>);
 };
--- src/TodoApp_mrsono0.test.js
+++ src/TodoApp_mrsono0.test.js
@@ -13,4 +13,12 @@ describe('<TodoApp_mrsono0 />', () => {
         getByText('TDD 배우기');
         getByText('react-testing-library');
     });
+    it('creates new todo', () => {
+        const { getByPlaceholderText, getByText } = render(<TodoApp_mrsono0 />);
+        fireEvent.change(getByPlaceholderText('할 일을 입력하세요'),
+            { target: { value: "새 항목 추가하기" } }
+        );
+        fireEvent.click(getByText('등록'));
+        getByText('새 항목 추가하기');
+    });
 });
