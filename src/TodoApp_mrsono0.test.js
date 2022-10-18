ab2af8bb90ddcc71cc1421bb7ba6febb5a1cb37d
mrsono0 <mrsono0@gmail.com>
2022-10-18 11:31:32 +0900

    TodoApp TodoForm TodoList render test ok

--- src/TodoApp_mrsono0.test.js
+++ src/TodoApp_mrsono0.test.js
@@ -8,4 +8,9 @@ describe('<TodoApp_mrsono0 />', () => {
         getByText('등록'); // TodoForm 화면에 있는지 확인
         getByTestId('TodoList'); // TodoList 화면에 있는지 확인
     });
+    it('renders two defaults todos', () => {
+        const { getByText } = render(<TodoApp_mrsono0 />);
+        getByText('TDD 배우기');
+        getByText('react-testing-library');
+    });
 });
