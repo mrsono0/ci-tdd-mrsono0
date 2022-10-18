b20d1b6d00e40fa17687ec36aeb368b54015ec16
mrsono0 <mrsono0@gmail.com>
2022-10-18 12:32:36 +0900

    TodoApp onRemove event test ok

--- src/TodoApp_mrsono0.js
+++ src/TodoApp_mrsono0.js
@@ -31,9 +31,14 @@ const TodoApp_mrsono0 = () => {
             todos.map(todo => todo.id === id ? { ...todo, done: !todo.done } : todo)
         );
     }, [todos]);
+    const onRemove = useCallback(id => {
+        setTodos(
+            todos.filter(todo => todo.id !== id)
+        );
+    }, [todos]);
     return (<div>
         <TodoForm onInsert={onInsert} />
-        <TodoList todos={todos} onToggle={onToggle} />
+        <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove} />
     </div>);
 };
 
